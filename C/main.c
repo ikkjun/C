198f1341a5986ca11782d318c98edd3761146534
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-03-14 20:46:48 +0900

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C
+//
+//  Created by Ikjun Cho on 3/14/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
