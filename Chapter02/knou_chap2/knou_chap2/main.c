29ce63df09cf1e14bad71235734a9a4f1c0234a7
ikjun <ikjun96@gmail.com>
2025-09-19 21:13:18 +0900

    c language class
    chap1

---
+++ Chapter02/knou_chap2/knou_chap2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  knou_chap2
+//
+//  Created by Ikjun Cho on 9/19/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
